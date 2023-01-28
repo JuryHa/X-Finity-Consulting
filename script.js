11dcd721b689af406db020772e0a44b098373951
JuryHa <juraj.hlavek@gmail.com>
2023-01-28 17:52:32 +0100

    added home menu

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 /* VARIABLES */
 const header = document.querySelector('header');
+const navButton = document.querySelector('header button');
+const navigation = document.querySelector('header nav');
 
 /* EVENT LISTENERS */
 window.addEventListener('scroll', function () {
   let scroll = window.scrollY;
   if (scroll > 60) {
-    header.style.backgroundColor = 'white';
+    header.classList.add('scrolled-to-top');
     header.style.boxShadow = 'var(--box-shadow-100)'
   } else {
-    header.style.backgroundColor = 'transparent';
+    header.classList.remove('scrolled-to-top');
     header.style.boxShadow = 'none'
   }
 });
+
+navButton.addEventListener('click', function () {
+  if (header.classList.contains('navigation-opened')) {
+    header.classList.remove('navigation-opened');
+  } else {
+    header.classList.add('navigation-opened');
+  }
+})
