eed139edb8fd7e8849a377e76d083dc179d560eb
JuryHa <juraj.hlavek@gmail.com>
2023-01-29 21:22:00 +0100

    menu burger handling

--- script.js
+++ script.js
@@ -3,32 +3,41 @@ const body = document.querySelector('html');
 const header = document.querySelector('header');
 const navButton = document.querySelector('header button');
 const navigation = document.querySelector('header nav');
+const menuItems = document.querySelectorAll('header ul a');
 
 /* EVENT LISTENERS */
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
-  if (scroll > 60) {
-    header.classList.add('scrolled-to-top');
-    header.style.boxShadow = 'var(--box-shadow-100)'
-  } else {
+  if (scroll < 60) {
     header.classList.remove('scrolled-to-top');
     header.style.boxShadow = 'none'
+  } else {
+    header.classList.add('scrolled-to-top');
+    header.style.boxShadow = 'var(--box-shadow-100)'
   }
 });
 
 navButton.addEventListener('click', () => {
-
   const isOpened = navButton.getAttribute('aria-expanded') === "true";
   if (isOpened ? closeMenu() : openMenu());
-
-  navButton.classList.toggle('open');
-  navigation.classList.toggle('nav--open');
+  header.classList.add('scrolled-to-top');
+  header.style.boxShadow = 'var(--box-shadow-100)'
 })
+
+menuItems.forEach(menuItem => {
+  menuItem.addEventListener('click', () => {
+    closeMenu();
+  })
+});
 
 function openMenu() {
   navButton.setAttribute('aria-expanded', 'true');
+  navButton.classList.toggle('open');
+  navigation.classList.toggle('nav--open');
 }
 
 function closeMenu() {
   navButton.setAttribute('aria-expanded', 'false');
+  navButton.classList.toggle('open');
+  navigation.classList.toggle('nav--open');
 }
