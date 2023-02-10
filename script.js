30969a81dc9cf3a03e19f1d9f52f21e60f78e9a6
JuryHa <juraj.hlavek@gmail.com>
2023-02-10 19:02:26 +0100

    redesigned colors

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const navButton = document.querySelector('header button');
 const navigation = document.querySelector('header nav');
 const menuItems = document.querySelectorAll('header ul a');
 
+const headingText = document.querySelector('h1');
+
 /* EVENT LISTENERS */
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
@@ -15,6 +17,8 @@ window.addEventListener("scroll", () => {
     header.classList.add('scrolled-to-top');
     header.style.boxShadow = 'var(--box-shadow-100)'
   }
+
+  headingText.style.textShadow = `0 ${(scroll / 2) + 4}px ${(scroll / 64) + 1}px rgb(132, 66, 232, ${1 - scroll * 0.005})`;
 });
 
 navButton.addEventListener('click', () => {
